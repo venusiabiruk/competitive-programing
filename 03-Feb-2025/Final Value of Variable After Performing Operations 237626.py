5949031d8e905c1e363664334b28c9accaab6a70
venusiabiruk <167543381+venusiabiruk@users.noreply.github.com>
2025-02-03 22:58:13 +0300

    Created 'Final Value of Variable After Performing Operations' by A2SV Hub

---
+++ 03-Feb-2025/Final Value of Variable After Performing Operations 237626.py
@@ -0,0 +1,12 @@
+# Problem: Final Value of Variable After Performing Operations - https://leetcode.com/problems/final-value-of-variable-after-performing-operations
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        x = 0
+        for i in operations:
+            if (i == "--X") or (i == "X--"):
+                x -=1
+            elif (i == "++X") or (i == "X++"):
+                x+=1
+        return x
+        
