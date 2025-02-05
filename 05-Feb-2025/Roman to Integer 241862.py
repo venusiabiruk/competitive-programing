fe790a85f9a94250a6c38ad81140847ac1019360
venusiabiruk <167543381+venusiabiruk@users.noreply.github.com>
2025-02-05 06:10:36 +0300

    Created 'Roman to Integer' by A2SV Hub

---
+++ 05-Feb-2025/Roman to Integer 241862.py
@@ -0,0 +1,20 @@
+# Problem: Roman to Integer - https://leetcode.com/problems/roman-to-integer/?envType=problem-list-v2&envId=string
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        d = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        sum = 0
+        n = len(s)
+        i = 0
+        while i < n: 
+            if i < (n-1) and d[s[i]] < d[s[i+1]]:
+                sum += d[s[i+1]] - d[s[i]]
+                i += 2
+            else:
+                sum += d[s[i]]
+                i +=1
+        return sum
+
+
+
+        
