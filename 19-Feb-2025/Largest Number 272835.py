2ddf87d12388a27784f00e0bb1ea9eb7278369bf
Venusia biruk <venusiabiruk16@gmail.com>
2025-02-19 08:19:19 +0300

    Created 'Largest Number' by A2SV Hub

---
+++ 19-Feb-2025/Largest Number 272835.py
@@ -0,0 +1,11 @@
+# Problem: Largest Number - https://leetcode.com/problems/largest-number/
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if f"{nums[j]}{nums[i]}" > f"{nums[i]}{nums[j]}":
+                    nums[i],nums[j] = nums[j],nums[i]
+        if nums[0] ==0:
+            return "0"
+        return "".join(map(str,nums))
