bcbfc5ddf6b9ee3aca491efbb639e8f7350bfea5
Venusia biruk <venusiabiruk16@gmail.com>
2025-02-26 01:40:18 +0300

    Created '3Sum Closest ' by A2SV Hub

---
+++ 25-Feb-2025/3Sum Closest  282956.py
@@ -0,0 +1,24 @@
+# Problem: 3Sum Closest  - https://leetcode.com/problems/3sum-closest/
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        n = len(nums)
+        c_sum = float(inf)
+        for i in range(n):
+            lo , hi = i+1, n-1
+            while lo < hi:
+                cur_sum = nums[i] + nums[lo] + nums[hi]
+                if abs(target-cur_sum) < abs(target-c_sum):
+                    c_sum = cur_sum
+                if cur_sum == target:
+                    return cur_sum
+                elif cur_sum < target:
+                    lo += 1
+                else:
+                    hi-=1
+        return c_sum 
+
+
+
+        
