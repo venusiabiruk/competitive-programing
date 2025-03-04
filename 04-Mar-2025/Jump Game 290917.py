902b4b081100d36b4734dfc26aeb4bb732ec81dc
Venusia biruk <venusiabiruk16@gmail.com>
2025-03-04 14:23:15 +0300

    Created 'Jump Game' by A2SV Hub

---
+++ 04-Mar-2025/Jump Game 290917.py
@@ -0,0 +1,12 @@
+# Problem: Jump Game - https://leetcode.com/problems/jump-game/
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        end = 0 
+        for idx , num in enumerate(nums):
+            if idx > end:
+                return False
+            end = max(end, idx +num)
+        return True
+
+        
