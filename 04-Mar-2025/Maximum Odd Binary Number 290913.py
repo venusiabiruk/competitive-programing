71c9e6056dd11ae631c1c8907d9542d8691f9a3e
Venusia biruk <venusiabiruk16@gmail.com>
2025-03-04 14:18:14 +0300

    Created 'Maximum Odd Binary Number' by A2SV Hub

---
+++ 04-Mar-2025/Maximum Odd Binary Number 290913.py
@@ -0,0 +1,13 @@
+# Problem: Maximum Odd Binary Number - https://leetcode.com/problems/maximum-odd-binary-number/
+
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        c_1 = s.count("1")
+        c_0 = len(s) - c_1
+        ans = []
+        for i in range((c_1-1)):
+            ans.append("1")
+        for j in range(c_0):
+            ans.append("0")
+        ans.append("1")
+        return("".join(ans))
