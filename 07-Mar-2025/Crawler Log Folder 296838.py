ceb76b55f9ac46b4404fb7658849b421fd1d4219
Venusia biruk <venusiabiruk16@gmail.com>
2025-03-07 17:57:51 +0300

    Created 'Crawler Log Folder' by A2SV Hub

---
+++ 07-Mar-2025/Crawler Log Folder 296838.py
@@ -0,0 +1,16 @@
+# Problem: Crawler Log Folder - https://leetcode.com/problems/crawler-log-folder/
+
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        stack = []
+        for i in logs:
+            if stack and i == "../":
+                stack.pop()
+            elif i != "./" and i != "../":
+                stack.append(i)
+        return len(stack)
+
+
+        
+        
+        
