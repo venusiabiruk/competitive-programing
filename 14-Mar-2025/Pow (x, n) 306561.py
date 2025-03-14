e8149f34f91c23fd61120f5434fa87781abf4819
Venusia biruk <venusiabiruk16@gmail.com>
2025-03-14 16:24:57 +0300

    Created 'Pow (x, n)' by A2SV Hub

---
+++ 14-Mar-2025/Pow (x, n) 306561.py
@@ -0,0 +1,7 @@
+# Problem: Pow (x, n) - https://leetcode.com/problems/powx-n/
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        result = (x**n)
+        return result
+        
