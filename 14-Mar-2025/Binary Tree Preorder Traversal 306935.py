82bee7b20ab0e8864d94ebf84f4a276e6259ce21
Venusia biruk <venusiabiruk16@gmail.com>
2025-03-14 19:06:11 +0300

    Created 'Binary Tree Preorder Traversal' by A2SV Hub

---
+++ 14-Mar-2025/Binary Tree Preorder Traversal 306935.py
@@ -0,0 +1,24 @@
+# Problem: Binary Tree Preorder Traversal - https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        def helper(node):
+            if not node:
+                return 
+            res.append(node.val)
+            helper(node.left)
+            helper(node.right)
+        helper(root)
+        return res
+
+
+    
+
+        
