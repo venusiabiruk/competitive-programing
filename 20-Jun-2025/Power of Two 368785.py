bcea7d9ecd3945addf6587d21c811311107c33b4
Venusia biruk <venusiabiruk16@gmail.com>
2025-06-20 14:49:46 -0700

    Created 'Power of Two' by A2SV Hub

---
+++ 20-Jun-2025/Power of Two 368785.py
@@ -0,0 +1,12 @@
+# Problem: Power of Two - https://leetcode.com/problems/power-of-two/
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        if n == 1:
+            return True
+        if n%2 != 0:
+            return False
+        return self.isPowerOfTwo(n/2)
+        
