7dbd8308358f5b28d4ca522e70b9a942950ae6df
Venusia biruk <venusiabiruk16@gmail.com>
2025-10-17 11:47:46 -0700

    Created 'N-th Tribonacci Number' by A2SV Hub

---
+++ 17-Oct-2025/N-th Tribonacci Number 404121.py
@@ -0,0 +1,10 @@
+# Problem: N-th Tribonacci Number - https://leetcode.com/problems/n-th-tribonacci-number/description/
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        t = [0,1,1]
+        if n < 3:
+            return t[n]
+        for i in range(3,n+1):
+            t[0],t[1],t[2] = t[1],t[2],sum(t)
+        return t[-1]
